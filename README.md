545634373368a6e3e3a6be6643899c3d7bd714d8
mati.gutierrez <mati.gutierrez@globant.com>
2018-03-22 10:26:43 -0300

    Add create_bs4_project.sh

--- README.md
+++ README.md
@@ -9,3 +9,21 @@ getopts_base.sh: basic example for using getopts in bash. With both short and lo
 passed to script.
 
 
+create_bs4_project.sh:
+=====================
+
+This script will create a basic bootstrap 4 template to start working from.
+Bootstrap template (index.html) and bootstrap files will be installed on you current
+working directory.
+
+create_bs4_project.sh --local-files 
+
+Will download both bootstrap and jquery and copy them to you current directory.
+
+and:
+
+create_bs4_project.sh --use-cdn
+
+Will create index.html and use bootstrap and jquery from its CDNs.
+
+
---
+++ get_bootstrap/create_bs4_project.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+
+
+function usage {
+    echo "This script will create a bootstrap 4 project in your current directory."
+    echo "You can select whether to use bootstrap files locally or to use cdn."
+    echo "Usage:"
+    echo
+    echo "${0} --local-files | --cdn"
+
+}
+
+
+function use_dist_files {
+    echo "[info] Downloading bootstrap"
+    if [[ ! -f /tmp/bootstrap.zip ]]
+    then
+        wget -O /tmp/bootstrap.zip http://getbootstrap.com/2.3.2/assets/bootstrap.zip
+    else
+        echo "[info] File already exists, I won't download it."
+    fi
+    if [[ ! -f /tmp/jquery-migrate-3.0.0.min.js ]]
+    then
+        wget -O /tmp/jquery-migrate-3.0.0.min.js https://code.jquery.com/jquery-migrate-3.0.0.min.js
+    else
+        echo "[info] Jquery file already exists, I won't download it."
+    fi
+    wget -O /tmp/index.html https://raw.githubusercontent.com/BlackrockDigital/startbootstrap-bare/master/index.html 
+    pushd /tmp
+    unzip -o bootstrap.zip
+    popd
+    mkdir -p vendor/bootstrap
+    mkdir -p vendor/jquery
+    for each in `ls /tmp/bootstrap`
+    do
+        cp -a /tmp/bootstrap/${each} vendor/bootstrap
+    done
+    cp /tmp/jquery-migrate-3.0.0.min.js vendor/jquery/jquery.min.js
+    cp /tmp/index.html .   
+    sed -i 's/bootstrap\.bundle\.min\.js/bootstrap\.min\.js/g' index.html
+    pwd
+    create_index_local
+}
+
+
+function release_resources {
+    rm -f /tmp/index.html
+    rm -f /tmp/bootstrap.zip
+    rm -f /tmp/jquery-migrate-3.0.0.min.js
+}
+
+
+function use_cdn {
+    tee index.html <<EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- If you wanna use jquery over CDN -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
+EOF
+
+
+
+}
+
+
+function create_index_local {
+    tee index.html <<EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <h1>Hello, world!</h1>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- If you wanna use jquery over CDN -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
+EOF
+}
+
+
+function main {
+    if [[ -z ${1} ]]
+    then
+        usage
+        exit 1
+    fi
+    TYPE="${1}"
+    case "${TYPE}" in
+        --local-files)
+            use_dist_files
+            ;;
+        --cdn)
+            use_cdn
+            ;;
+        *)
+            usage
+            exit 1
+    esac
+}
+
+
+main $*
+trap release_resources EXIT
+
